bee04c9991c0ca300c8a8bd39f8eca506237a308
DaviZzzz <davgar21@ucm.es>
2021-11-28 14:27:35 +0100

    aaa

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,25 +23,5 @@ module.exports = {
                 response.redirect("/");
             }
         });
-function cb_isUser(err, result) {
-    if (err) {
-
-    } else if (!result) {
-
-    } else {
-        request.session.email = result[0].email;
-        request.session.password = request.body.password;
-
-        response.redirect("/");
     }
-}
-
-module.exports = {
-    login(request, response) {
-        response.render("login");
-    },
-    isUser(request, response) {
-        DAOUser.isUser(request.body.user, request.body.password, cb_isUser);
-    }
-
 };
