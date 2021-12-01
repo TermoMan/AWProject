225ead2d0d83e33d38be37c0c4c3cda3338a61c8
DaviZzzz <davgar21@ucm.es>
2021-12-01 14:18:31 +0100

    empezando con register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const DAOUser = require("../model/DAOUser");
-
+const fs = require('fs');
 const DAOUserr = new DAOUser();
 
 
@@ -23,5 +23,35 @@ module.exports = {
                 response.redirect("/");
             }
         });
+    },
+    doRegister(request, response){
+        response.render("registration", { error: null });
+    },
+    register(request, response){
+        /*var img = request.body.img;
+        if(img === ""){
+            console.log("sasasa");
+            fs.readdir("\images", function(err, files){
+                if(err) console.log(err);
+                else {
+                    var random = files[Math.floor(Math.random()*files.length)];
+
+                }   
+            })
+        }
+        DAOUserr.register(request.body.user, request.body.password, function(err, result) {
+            if (err) {
+                console.log(err);
+                response.render("login", { error: "Error interno de acceso a la base de datos" });
+            } else if (!result) {
+                response.render("login", { error: "No existe el usuario" });
+            } else {
+                console.log(result);
+                request.session.email = result[0].correo;
+                request.session.password = request.body.password;
+                console.log("sasasasa");
+                response.redirect("/");
+            }
+        });*/
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const usersController = require("../controllers/userController");
 
 router.get("/login", usersController.login);
-router.post("/login", usersController.isUser)
+router.post("/login", usersController.isUser);
+
+router.get("/doRegister", usersController.doRegister);
+router.post("/register", usersController.register);
 
 module.exports = router;
