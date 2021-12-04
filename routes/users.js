e1040cac7f9ea372a297d374126a6086319b04c6
TermoMan <80047050+TermoMan@users.noreply.github.com>
2021-12-04 21:03:17 +0100

    cerrar sesion en index hecho

--- routes/users.js
+++ routes/users.js
@@ -7,67 +7,73 @@ const DAOUserr = new DAOUser();
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: './public/uploads/',
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
     }
-  })
-  
+})
+
 const upload = multer({ storage: storage })
 const usersController = require("../controllers/userController");
 
 router.get("/login", function(request, response) {
-  response.render("login", { error: null });
+    response.render("login", { error: null });
 });
+
+router.get("/logout", function(request, response) {
+    request.session.destroy();
+    response.render('login', { error: null });
+})
+
 router.post("/login", function(request, response, next) {
-  DAOUserr.isUser(request.body.user, request.body.password, function(err, result) {
-      if (err) {
-          console.log(err);
-          response.render("login", { error: "Error interno de acceso a la base de datos" });
-      } else if (!result) {
-          response.render("login", { error: "No existe el usuario" });
-      } else {
-          request.session.email = result[0].correo;
-          request.session.password = request.body.password;
-          request.session.name = result[0].nickname;
-          request.session.userId = result[0].id;
-          request.session.image = result[0].imagen;
-          request.session.date = result[0].fecha;
-          request.session.reputation = result[0].reputacion;
+    DAOUserr.isUser(request.body.user, request.body.password, function(err, result) {
+        if (err) {
+            console.log(err);
+            response.render("login", { error: "Error interno de acceso a la base de datos" });
+        } else if (!result) {
+            response.render("login", { error: "No existe el usuario" });
+        } else {
+            request.session.email = result[0].correo;
+            request.session.password = request.body.password;
+            request.session.name = result[0].nickname;
+            request.session.userId = result[0].id;
+            request.session.image = result[0].imagen;
+            request.session.date = result[0].fecha;
+            request.session.reputation = result[0].reputacion;
 
-          response.redirect("/");
-      }
-  });
+            response.redirect("/");
+        }
+    });
 });
 
-router.get("/doRegister", function(request, response){
-  request.session.destroy();
-  response.render("registration", { error: null });
+router.get("/doRegister", function(request, response) {
+    request.session.destroy();
+    response.render("registration", { error: null });
 });
-router.post("/register", upload.single("img"), function(request, response){
-  request.session.destroy();
-  var img = request.file;
-  let imagen = null;
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  if(img === undefined){
-      var dir = './images/';
-      var dir2 = './public/uploads/';
-      var files = fs.readdirSync(dir);
-      imagen = files[Math.floor(Math.random()*files.length)];
-      fs.copyFile(dir += imagen, dir2 += imagen, (err) => {
-          if (err) throw err;
-      });
-  } else imagen = img.originalname;
-  DAOUserr.register(request.body.user, request.body.password, request.body.nickname, imagen, date, function(err, result) {
-      if (err) {
-          console.log(err);
-          response.render("registration", { error: "Error interno de acceso a la base de datos" });
-      } else if (!result) {
-          response.render("registration", { error: "Ya existe el usuario" });
-      } else {
-          response.redirect("/users/login");
-      }
-  });
+router.post("/register", upload.single("img"), function(request, response) {
+    request.session.destroy();
+    var img = request.file;
+    let imagen = null;
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    if (img === undefined) {
+        var dir = './images/';
+        var dir2 = './public/uploads/';
+        var files = fs.readdirSync(dir);
+        imagen = files[Math.floor(Math.random() * files.length)];
+        fs.copyFile(dir += imagen, dir2 += imagen, (err) => {
+            if (err) throw err;
+        });
+    } else imagen = img.originalname;
+    DAOUserr.register(request.body.user, request.body.password, request.body.nickname, imagen, date, function(err, result) {
+        if (err) {
+            console.log(err);
+            response.render("registration", { error: "Error interno de acceso a la base de datos" });
+        } else if (!result) {
+            response.render("registration", { error: "Ya existe el usuario" });
+        } else {
+            response.redirect("/users/login");
+        }
+    });
 });
 
 module.exports = router;
