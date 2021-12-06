d8a21452c63e6ae8ea2b45e46e522e090687d707
TermoMan <80047050+TermoMan@users.noreply.github.com>
2021-12-06 19:14:38 +0100

    eliminado console log

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,15 @@ const DAOQuest = require("../model/DAOQuest");
 const fs = require('fs');
 const DAOQuestt = new DAOQuest();
 
-function convertirFecha (fechaString) {
+function convertirFecha(fechaString) {
     var fechaSp = fechaString.split("-");
     var anio = new Date().getFullYear();
     anio = fechaSp[2];
     var mes = fechaSp[1] - 1;
     var dia = fechaSp[0];
-  
+
     return new Date(anio, mes, dia);
-  }
+}
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
@@ -21,19 +21,19 @@ router.get('/', function(request, response, next) {
     response.render('index');
 });
 
-router.get('/showQuestions', function(request, response){
+router.get('/showQuestions', function(request, response) {
     console.log(response.locals);
     DAOQuestt.getQuestions(function(err, result) {
         let quests = new Array();
         if (err) {
             console.log(err);
-            response.render("allQuest", {quests, error: "Error interno de acceso a la base de datos"});
+            response.render("allQuest", { quests, error: "Error interno de acceso a la base de datos" });
         } else if (!result) {
-            response.render("allQuest", {quests, error: null});
+            response.render("allQuest", { quests, error: null });
         } else {
             result.forEach(e => {
                 d = new Date(e.fecha);
-                var datestring = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear();
+                var datestring = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
                 if (quests.length === 0) quests.push({
                     id: e.idpregunta,
                     titulo: e.titulo,
@@ -43,7 +43,7 @@ router.get('/showQuestions', function(request, response){
                     imagen: e.imagen,
                     tags: [e.texto]
                 });
-                else{
+                else {
                     if (quests[quests.length - 1].id === e.idpregunta) quests[quests.length - 1].tags.push(e.texto);
                     else quests.push({
                         id: e.idpregunta,
@@ -56,21 +56,21 @@ router.get('/showQuestions', function(request, response){
                     });
                 }
             });
-            console.log(quests.sort(function (a, b) { 
-                return convertirFecha(b.fecha) - convertirFecha(a.fecha) ; 
-              }));
-            response.render("allQuest", {quests, error: null});
+            quests.sort(function(a, b) {
+                return convertirFecha(b.fecha) - convertirFecha(a.fecha);
+            });
+            response.render("allQuest", { quests, error: null });
         }
     });
-    
+
 });
 
-router.get('/formQuest', function(request, response){
+router.get('/formQuest', function(request, response) {
     console.log(response.locals);
     response.render("newQuest", { error: null });
 });
 
-router.post('/insertQuest', function(request, response){
+router.post('/insertQuest', function(request, response) {
     var lbls = request.body.labels;
     console.log(response.locals);
     lbls = lbls.split("@");
